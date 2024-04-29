69f4503f115dda23eb87d93000a6d2a0f6ed913a
stephane-klein <contact@stephane-klein.info>
2024-04-29 23:04:25 +0200

    WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,7 @@
     let _codeEditorView;
 
     const items = [
+        {label: "[[ab", detail: "Detail about Item0"},
         {label: "Item1", detail: "Detail about Item1"},
         {label: "Item2", detail: "Detail about Item2"},
         {label: "Item3", detail: "Detail about Item3"}
@@ -19,11 +20,13 @@
 
     function complete(context) {
         let before = context.matchBefore(/\[\[$/);
-        if (!before) return null;
+        if (!before) return null;  // Rien si le déclencheur '[[ 'n'est pas présent
+        console.log("ici1");
 
         return {
             from: before.from,
             options: items.map(item => ({label: item.label, detail: item.detail, type: "text"})),
+            filter: false,
             validFor: /^[\w\s]*$/
         };
     }
@@ -36,7 +39,8 @@
                 CodeBasicSetup,
                 languageConf.of(markdown()),
                 autocompletion({
-                    override: [complete]
+                    override: [complete],
+                    activateOnTyping: true
                 }),
                 keymap.of([...completionKeymap])
             ]
